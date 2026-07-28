861c5368984201bdc7e411e6123f55cff6110bc9
Raffael Dela Cruz <160813753+rafdelacruz@users.noreply.github.com>
2026-07-27 20:45:22 -0400

    Fix path issues for DLLs and SDK files

--- src/test_thorlabs.py
+++ src/test_thorlabs.py
@@ -25,7 +25,7 @@ def configure_path():
 
     """
     is_64bits = sys.maxsize > 2**32
-    relative_path_to_dlls = os.sep + 'dlls' + os.sep
+    relative_path_to_dlls = os.sep + '..' + os.sep + 'dlls' + os.sep
 
     if is_64bits:
         relative_path_to_dlls += '64_lib'
